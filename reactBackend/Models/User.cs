ac644bd0cb66cf0b5d9d2cf3509e5b6c3cbb7fa3
Theo Versluys <kanbikil@gmail.com>
2024-04-30 14:27:38 +0200

    modif class

--- reactBackend/Models/User.cs
+++ reactBackend/Models/User.cs
@@ -9,7 +9,7 @@ namespace reactBackend.Models
         public string? LastName { get; set; }
         public string? FirstName { get; set; }
         public string? Email { get; set; }
-        public string? Password { get; set; }
+        //public string? Password { get; set; } // Trouvez le moyen d'utiliser un mot de passe sécurisé sans le mettre dans la classe
         public string? PhoneNumber { get; set; }
     }
 
