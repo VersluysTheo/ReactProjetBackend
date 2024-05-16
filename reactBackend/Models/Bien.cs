c1206e6cb0600101c9b40a04897ce661cae2bd82
Theo Versluys <kanbikil@gmail.com>
2024-05-16 19:33:18 +0200

    Modif class bien

--- reactBackend/Models/Bien.cs
+++ reactBackend/Models/Bien.cs
@@ -5,12 +5,12 @@
         public int BienId { get; set; }
         public string? Quartier { get; set; }
         public int? Prix { get; set; }
-        public int? NbrChambres { get; set; }
+        public int? Nbr_Chambres { get; set; }
         public int? Nbr_Pieces { get; set; }
-        public string? ClasseEnergetique { get; set; }
+        public string? Classe_Energetique { get; set; }
         public long? Description { get; set; }
-        public int? SurfaceHabitable { get; set; }
-        public int? SurfaceTotale { get; set; }
+        public int? Surface_Habitable { get; set; }
+        public int? Surface_Totale { get; set; }
 
         // Propriété de navigation vers l'utilisateur associé
         public ICollection<User> Users { get; set; }
